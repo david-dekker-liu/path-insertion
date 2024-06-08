4abd3d47fbb3bfe3994d5c3de2ef5feca958c2bf
davde78 <david.dekker@liu.se>
2024-06-08 13:19:41 +0200

    Proper interval representations when printed

--- src/intervals.py
+++ src/intervals.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class LinkedInterval:
     def __init__(self, start, end, orig_start, orig_end):
         self.start = start
@@ -10,8 +13,20 @@ class LinkedInterval:
             return self.__dict__ == other.__dict__
         return False
 
+    def __repr__(self):
+        return f"({self.start.strftime('%Y-%m-%d %H:%M:%S')}, {self.end.strftime('%Y-%m-%d %H:%M:%S')}, {self.orig_start.strftime('%Y-%m-%d %H:%M:%S')}, {self.orig_end.strftime('%Y-%m-%d %H:%M:%S')})"
+
+    def __str__(self):
+        return f"({self.start.strftime('%Y-%m-%d %H:%M:%S')}, {self.end.strftime('%Y-%m-%d %H:%M:%S')}, {self.orig_start.strftime('%Y-%m-%d %H:%M:%S')}, {self.orig_end.strftime('%Y-%m-%d %H:%M:%S')})"
+
 
 class Interval:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
+    def __repr__(self):
+        return f"({self.start.strftime('%Y-%m-%d %H:%M:%S')}, {self.end.strftime('%Y-%m-%d %H:%M:%S')})"
+
+    def __str__(self):
+        return f"({self.start.strftime('%Y-%m-%d %H:%M:%S')}, {self.end.strftime('%Y-%m-%d %H:%M:%S')})"
